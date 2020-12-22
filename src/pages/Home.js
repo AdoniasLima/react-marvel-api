f5bc818658558b353b6bd7aadfb797cf09463c98
AdoniasLima <adoniaslimagouveia@outlook.com>
2020-12-22 15:55:58 -0300

    Revert "[Feature] Creating generate of link with timestamp, public key and private key"
    This reverts commit 9d38feade31d83d8ae73544d8725c6b51a4578f5.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from "react";
-import api from "../services/api";
-import generateLink from "../services/generate";
+import React from "react";
 
 function Home(){
-
-    const [characters, setCharacters] = useState([]);
-
-    useEffect(() => {
-        api.get("/characters?limit=18&" + generateLink()).then(response => {
-            setCharacters(response.data.data.results);
-        });
-    }, []);
-
     return(
-        <div>
-            <h1>Home</h1>
-            <ul>
-                {characters.map(function(value, index){
-                    return <li key={index}>{value.name}</li>
-                })}
-            </ul>
-        </div>
+        <h1>Home</h1>
     );
 }
 
